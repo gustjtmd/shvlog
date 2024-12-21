0390230a04a941920a5b40fdf408601c4bc70dee
gustjtmd <shseo@datanuri.net>
2024-12-21 19:14:48 +0900

    Study

---
+++ src/main/java/com/example/shvlog/ShvlogApplication.java
@@ -0,0 +1,13 @@
+package com.example.shvlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShvlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShvlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/shvlog/ShvlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shvlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShvlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
