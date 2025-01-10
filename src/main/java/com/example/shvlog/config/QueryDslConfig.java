2f39782f8c0f5638a13b0e3dfb3e0795dbe5a826
gustjtmd <gustjtmd@gmail.com>
2025-01-10 20:47:18 +0900

    commit

--- src/main/java/com/example/shvlog/config/QueryDslConfig.java
+++ src/main/java/com/example/shvlog/config/QueryDslConfig.java
@@ -10,8 +10,11 @@ import org.springframework.context.annotation.Configuration;
 public class QueryDslConfig {
 
 
+
     @PersistenceContext
     public EntityManager em;
+
+
 
     @Bean
     public JPAQueryFactory jpaQueryFactory() {
